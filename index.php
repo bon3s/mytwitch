659689962d2c36753f843858225fc6b2ccfe5bf3
bon3s <marko.suvala@gmail.com>
2016-04-01 01:36:44 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>myTwitch</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+  <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css'>
+  
+</head>
+
+
+<body>
+
+  <div class="container scotch-panel" id="super-container">
+
+
+    <div class="left-menu-wrapper">
+      <div class="left-menu-container">
+
+        <div class="left-menu-item" id="english"><span class="language">English</span>
+          <div class="checkboxes">
+
+            <input type="checkbox" id="c1" />
+            <label for="c1"><div></div></label>
+          </div>
+        </div>
+
+        <div class="left-menu-item" id="german"><span class="language">German</span>
+          <div class="checkboxes">
+
+            <input type="checkbox" id="c2" />
+            <label for="c2"><div></div></label>
+          </div>
+        </div>
+
+        <div class="left-menu-item" id="croatian"><span class="language">Croatian</span>
+          <div class="checkboxes">
+
+            <input type="checkbox" id="c3" />
+            <label for="c3"><div></div></label>
+          </div>
+        </div>
+
+        <div class="left-menu-item" id="italian"><span class="language">Italian</span>
+          <div class="checkboxes">
+
+            <input type="checkbox" id="c4" />
+            <label for="c4"><div></div></label>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="left-button-column"><i class="fa fa-filter toggle-left" id="buttonleft"></i></div>
+
+    <div class="content-wrapper" >
+      
+      
+
+      <div class="header-wrapper">
+
+        <div class="header">
+          <div class="headertitle">
+            <div class="whitetitle">TWITCH</div><i class="glyphtitle fa fa-play-circle"></i>
+            <div class="bluetitle">TV</div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="streams-wrapper">
+       
+
+      </div>
+      <!--End Stream-wrapper-->
+
+      <div class="loadmore">
+          <div id="loadbutton">
+            <div id="buttontext">Load<br>More
+              </div>
+          </div>
+        </div>
+
+
+    </div>
+    <!--End Content wrapper-->
+
+    <div class="right-button-column">
+        <i class="glyphicon glyphicon-menu-hamburger toggle-right" id="buttonright"></i>
+
+    </div>
+
+
+
+
+
+
+
+    <div class="right-menu-wrapper">
+
+      <div class="right-menu-container">
+
+        <div class="right-menu-item" id="leagueoflegends">League of legends</div>
+        <div class="right-menu-item" id="dota2">Dota 2</div>
+        <div class="right-menu-item" id="hearthstone">Hearthstone</div>
+        <div class="right-menu-item" id="counterstrike">Counter<span id="minus"> - </span>Strike</div>
+        <div class="right-menu-item" id="call">Call of Duty</div>
+        <div class="right-menu-item" id="minecraft">Minecraft</div>
+        <div class="right-menu-item" id="overwatch">Overwatch</div>
+        <div class="right-menu-item" id="xcom">XCOM 2</div>
+        <div class="right-menu-item" id="starcraft">Starcraft II</div>
+
+
+
+      </div>
+
+    </div>
+
+      <!-- Include jQuery -->
+
+      <script src="//cdn.jsdelivr.net/jquery/2.2.0/jquery.min.js"></script>
+      <script src="js/scotchPanels.js"></script>
+      <script src="js/heartbeat.js"></script>   
+      <script src="http://momentjs.com/downloads/moment.min.js" async></script>
+
+      <script type="text/javascript">
+      $('.left-menu-wrapper').scotchPanel({
+        containerSelector: 'body', // As a jQuery Selector
+        direction: 'left', // Make it toggle in from the left
+        duration: 200, // Speed in ms how fast you want it to be
+        transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
+        clickSelector: '.toggle-left', // Enables toggling when clicking elements of this class
+        distanceX: '400px', // Size fo the toggle
+        enableEscapeKey: true // Clicking Esc will close the panel
+      });
+
+      $('.right-menu-wrapper').scotchPanel({
+        containerSelector: 'body', // As a jQuery Selector
+        direction: 'right', // Make it toggle in from the right
+        duration: 200, // Speed in ms how fast you want it to be
+        transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
+        clickSelector: '.toggle-right', // Enables toggling when clicking elements of this class
+        distanceX: '400px', // Size fo the toggle
+        enableEscapeKey: true // Clicking Esc will close the panel
+      });
+
+    </script>
+
+    <script type="text/javascript">
+      $('#buttonleft').on('click', function() {
+        $(this)
+          .toggleClass('fa fa-filter')
+          .toggleClass('ion-close');
+
+
+
+      });
+
+    </script>
+
+    <script type="text/javascript">
+      $('#buttonright').on('click', function() {
+        $(this)
+          .toggleClass('glyphicon-menu-hamburger')
+          .toggleClass('ion-close');
+          
+
+      });
+    </script>
+    
+
+    
+
+
+</body>
+
+</html>
---
+++ js/heartbeat.js
@@ -0,0 +1,202 @@
+// Global variables
+
+// Checkbox variables
+
+var c1 = $("#c1");
+var c2 = $("#c2");
+var c3 = $("#c3");
+var c4 = $("#c4");
+// Game search and default url variables
+var api_base_url = "https://api.twitch.tv/kraken/streams";
+var api_search_url = "https://api.twitch.tv/kraken/search/streams?q=";
+var api_suggest_url_add = "&type=suggest";
+var game_filter = '';
+
+// Language filter variables
+var arrdata = '';
+var langarr = [];
+var en = "en";
+var de = "de";
+var hr = "hr";
+var it = "it";
+
+
+
+//Language filters (checkboxes)         
+$("#c1").on('click', function() {
+
+    $.each(arrdata.streams, function(index,item) {
+        if(item.channel.broadcaster_language === "en"){
+           langarr.push(item);
+
+
+        }
+        else{
+            langarr = [];
+        }
+        
+    });
+
+    
+});
+
+$("#c2").on('click', function() {
+
+        $.each(arrdata.streams, function(index,item) {
+        if(item.channel.broadcaster_language == de){
+           langarr.push(item);
+
+        }
+        else{
+            langarr = [];
+        }
+    });
+
+ 
+});
+
+$("#c3").on('click', function() {
+
+        $.each(arrdata.streams, function(index,item) {
+        if(item.channel.broadcaster_language == hr){
+           langarr.push(item);
+
+        }
+        else{
+            langarr = [];
+        }
+    });
+
+});
+
+$("#c4").on('click', function() {
+
+        $.each(arrdata.streams, function(index,item) {
+        if(item.channel.broadcaster_language == it){
+           langarr.push(item);
+
+        }
+        else{
+            langarr = [];
+        }
+    });
+
+
+});
+
+
+
+// Search by game
+
+$(".right-menu-item").on('click', function() {
+    game_filter = $(this).attr("id");
+    $(".stream").remove();
+    getstreams();
+});
+
+// Return to all streams
+
+$(".headertitle").on('click', function() {
+    game_filter = '';
+    $(".stream").remove();
+    getstreams();
+});
+
+//Ajax call as a function
+
+function getstreams() {
+  
+  if (game_filter == '') {
+    url = api_base_url;
+  } 
+  else if(game_filter == call){
+    url = api_search_url + game_filter + api_suggest_url_add;
+  }
+  else {
+    url = api_search_url + game_filter;
+  }
+
+//Ajax call, start render on success
+
+  $.ajax({
+    method: 'GET',
+    url: url,
+    data: {
+      
+      limit: 50
+    },
+    success: render
+
+  });
+  
+
+};
+
+function render(data) {
+    console.log(data);
+
+    //Store array 
+    arrdata = data;
+
+    // Generate containers
+    $.each(data.streams, function(index,item) {
+        var element="<div class=\"stream\"> <!--Stream row1element1--> <div class=\"stream-header\"> <!--Stream header--> <div class=\"avatar\" style=\"background-image: url('" + item.channel.logo + "')\" ></div> <div class=\"player-name\">" + item.channel.name + "</div> </div> <!--End Stream header--> <div class=\"stream-preview\"> <img src=" + item.preview.medium + "></img> <!--Stream preview--> </div> <!--End Stream preview--> <div class=\"stream-info\"> <!--Stream info--> <div class=\"gamename\">"+item.channel.game+"</div> <!--Game name--> <div class=\"viewdaterow\"> <!--ViewDateRow--> <div class=\"viewcount\"><i class=\"fa fa-eye\"></i>"+ item.viewers +" </div><i class=\"fa fa-circle\"></i> <!--ViewCount--> <div class=\"date\">" + item.created_at + "</div> <!--Date and time--> </div> <!--End ViewDateRow--> </div> <!--End Stream info--> </div>";
+        var num = $(".stream").length;
+        $('.streams-wrapper').append(element);         
+        
+
+    });
+      $('.date').each(function() {
+           var formatted = moment($(this).text(), 'YYYY-MM-DDThh:mm:ss').format('MMM DD, YYYY'); 
+           $(this).text(formatted);
+      });  
+};
+// Display streams on load
+getstreams(); 
+
+// Load and render more streams
+$("#loadbutton").on('click', function() {getstreams()});  // Display streams by clicking on Load more button
+
+
+
+
+
+
+
+
+    $(".streams-wrapper").on('click','.stream', function(){
+
+            
+            var streamurl = $(this).find('.player-name').text();
+            var videogamename = $(this).find('.gamename').text();
+            var videoviews = parseInt($(this).find('.viewcount').text(),10);
+            var videodate = $(this).find('.date').text();
+            var videoplayername = streamurl;
+            var videoavatar = $(this).find('.avatar').attr('background-image','url').text;
+            console.log(videoavatar);
+            console.log(streamurl);
+            var player = "<iframe src=\"http://player.twitch.tv/?channel=" + streamurl + "\" height=\"530\"width=\"930\"frameborder=\"0\"scrolling=\"no\"allowfullscreen=\"true\"> </iframe>"
+
+        
+        
+       
+            $(".stream").hide();
+            $("#loadbutton").hide();
+            $(".left-button-column").hide();
+            $(".right-button-column").hide();
+            $(".video").css('display','flex');
+            $(".streams-wrapper").css('justify-content','center');
+            $(".streams-wrapper").append("<div class=\"videopopup\"><div class=\"videoplayer\"><div class=\"video-container\"><div class=\"player\">" + player + "</div></div><div class=\"ion-android-close id=\"playerclose\"></div></div><div class=\"videoinfo\"><div class=\"videoinfoleft\"><div class=\"videogamename\">" + videogamename + "</div><div class=\"viewsanddate\"><div class=\"videoviews\"><i class=\"fa fa-eye\"></i><span>" + videoviews + "</span><i class=\"fa fa-circle\"></i><div class=\"videodate\"><span>"+ videodate +"</span></div></div></div></div></div>");
+
+           
+        });
+
+        $(".streams-wrapper").on('click','.ion-android-close',function(){
+            $('.videopopup').remove();
+
+            $("#loadbutton").show();
+            $(".left-button-column").show();
+            $(".right-button-column").show();
+            $(".streams-wrapper").css('justify-content','flex-start');
+            getstreams();
+        });
